a7548fb2cc0b3d3f90d03e9a6caaf9da90a7d548
Trần Đỗ Anh Khoa <anhkhoatrando123@gmail.com>
2022-08-28 15:14:19 +0700

    mobile responsive

---
+++ main.js
@@ -0,0 +1,8 @@
+function tripleBar() {
+    let x1 = document.querySelector(".nav_list-unshortened");
+    x1.classList.toggle("show");
+    let y = document.querySelector(".nav_list-triple");
+    y.classList.toggle("turn");
+    let y2 = document.querySelector(".nav_list-triple");
+    y2.classList.toggle("return");
+}
